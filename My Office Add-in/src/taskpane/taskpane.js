91f07d475d1c9c8c882eb0060fce531ebc23f085
David Ihl <dihl@becvie.loc>
2024-01-31 11:15:38 +0100

    Follow step 7 to 8

--- My Office Add-in/src/taskpane/taskpane.js
+++ My Office Add-in/src/taskpane/taskpane.js
@@ -9,5 +9,29 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
+    document.getElementById("create-table").onclick = () => tryCatch(createTable);
   }
 });
+
+async function createTable() {
+  await Excel.run(async (context) => {
+
+      // TODO1: Queue table creation logic here.
+
+      // TODO2: Queue commands to populate the table with data.
+
+      // TODO3: Queue commands to format the table.
+
+      await context.sync();
+  });
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+  try {
+      await callback();
+  } catch (error) {
+      // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+      console.error(error);
+  }
+}
